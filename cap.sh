fcf45ff86d704a93b8a8da8ced51797ea4620aa6
ko1596 <ko89022189@gmail.com>
2021-07-23 17:19:39 +0800

    fix capture format to YUYV

--- cap.sh
+++ cap.sh
@@ -6,7 +6,7 @@ do
     FILE0="/home/root/$NOW"
 
     echo $FILE0
-    v4l2-ctl -d /dev/video1 --set-parm=30 --set-fmt-video=width=1920,height=1080,pixelformat=MJPG --stream-mmap --stream-count=1 --stream-to=$NOW.jpg
+    v4l2-ctl -d /dev/video1 --set-parm=30 --set-fmt-video=width=1920,height=1080,pixelformat=YUYV --stream-mmap --stream-count=1 --stream-to=$FILE0.raw
     i=$(($i+1))
     sleep 1
 done
