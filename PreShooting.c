52f7bb1c2c5b59512caea8dedf91807c324afaff
ko1596 <ko89022189@gmail.com>
2021-07-27 10:15:50 +0800

    fix getobjectdata

--- PreShooting.c
+++ PreShooting.c
@@ -15,23 +15,23 @@ Radar_Error Radar_GetObjectStatus(Radar_PredictionData_t *pPredictionData, M0_RA
 {
 	Radar_Error Status = RADAR_ERROR_NONE;
 
-	if(data.obj_type == 6)
+	if (data.obj_type == 6)
 		Status = RADAR_ERROR_WORNG_DISTANCE;
 
 	if (Status == RADAR_ERROR_NONE)
 	{
 
-		if(data.obj_distance_R == 0)
+		if (data.obj_distance_R == 0)
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_EMPTY;
 		// else if(data.obj_distance_R > 0)
 		// 	pPredictionData->Status = RADAR_PREDICTIONSTATUS_COMING;
-		else if(pPredictionData->SpeedData.DeltaV < -5)
+		else if (pPredictionData->SpeedData.DeltaV < -5)
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_COMING;
-		else if(pPredictionData->SpeedData.DeltaV > 5)
+		else if (pPredictionData->SpeedData.DeltaV > 5)
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_LEAVING;
-		else if(pPredictionData->SpeedData.DeltaV <= 5 && pPredictionData->SpeedData.DeltaV >= -5)
+		else if (pPredictionData->SpeedData.DeltaV <= 5 && pPredictionData->SpeedData.DeltaV >= -5)
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_PARKING;
-		else if(M0_radarA.data.obj_type == 1)
+		else if (M0_radarA.data.obj_type == 1)
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_PARKED;
 		else
 			pPredictionData->Status = RADAR_PREDICTIONSTATUS_INVALID;
@@ -46,12 +46,19 @@ Radar_Error Radar_GetObjectSpeedData(Radar_PredictionData_t *pPredictionData, M0
 
 	if (Status == RADAR_ERROR_NONE)
 	{
-		
-	 	pPredictionData -> SpeedData.DeltaX = data.obj_distance_R - pPredictionData -> SpeedData.InitialDistance;
-		pPredictionData -> SpeedData.InitialDistance = data.obj_distance_R;
+		if (pPredictionData->SpeedData.InitialDistance != 0)
+			pPredictionData->SpeedData.DeltaX = data.obj_distance_R - pPredictionData->SpeedData.InitialDistance;
+		else 
+			pPredictionData->SpeedData.DeltaX = 0;
 
-		pPredictionData -> SpeedData.DeltaV = pPredictionData -> SpeedData.DeltaX - pPredictionData -> SpeedData.InitialSpeed;
-		pPredictionData -> SpeedData.InitialSpeed = pPredictionData -> SpeedData.DeltaX;
+		pPredictionData->SpeedData.InitialDistance = data.obj_distance_R;
+
+		if (pPredictionData->SpeedData.InitialSpeed != 0)
+			pPredictionData->SpeedData.DeltaV = pPredictionData->SpeedData.DeltaX - pPredictionData->SpeedData.InitialSpeed;
+		else
+			pPredictionData->SpeedData.DeltaV = 0;
+
+		pPredictionData->SpeedData.InitialSpeed = pPredictionData->SpeedData.DeltaX;
 	}
 
 	return Status;
@@ -66,7 +73,8 @@ void *Radar_TakePicture(void *parm)
 	if (access(path, F_OK) == -1)
 		Status = RADAR_ERROR_FILE_NOT_EXIST;
 
-	if(Status == RADAR_ERROR_NONE){
+	if (Status == RADAR_ERROR_NONE)
+	{
 		bzero(sysCmdBuf, 256);
 		sprintf(sysCmdBuf, path);
 		system(sysCmdBuf);
@@ -76,29 +84,30 @@ void *Radar_TakePicture(void *parm)
 	//return Status;
 }
 
-Radar_Error Radar_PrintData(Radar_PredictionData_t *pPredictionData, M0_RADAR_DATA_FRAME data){
+Radar_Error Radar_PrintData(Radar_PredictionData_t *pPredictionData, M0_RADAR_DATA_FRAME data)
+{
 	Radar_Error Status = RADAR_ERROR_NONE;
 
-	data.L_R ? PRINTLF(LEFT) : PRINTLF(RIGHT);		//PRINTLF(LF) printf("--------------------------#LF--------------------------\n\r")
+	data.L_R ? PRINTLF(LEFT) : PRINTLF(RIGHT); //PRINTLF(LF) printf("--------------------------#LF--------------------------\n\r")
 
 	printf("initX: %3d\tDeltaX: %2d\n\rinitV: %3d\tDeltaV: %2d\n\rStatus: %1d\n\rDistance: %3d\n\r",
-		pPredictionData ->SpeedData.InitialDistance,
-		pPredictionData->SpeedData.DeltaX,
-		pPredictionData->SpeedData.InitialSpeed,
-		pPredictionData->SpeedData.DeltaV,
-		pPredictionData->Status,
-		data.obj_distance_R
-		);
+		   pPredictionData->SpeedData.InitialDistance,
+		   pPredictionData->SpeedData.DeltaX,
+		   pPredictionData->SpeedData.InitialSpeed,
+		   pPredictionData->SpeedData.DeltaV,
+		   pPredictionData->Status,
+		   data.obj_distance_R);
 	printf("--------------------------------------------------------\n\r");
 	return Status;
 }
 
-Radar_Error Radar_InitData(Radar_PredictionData_t *pPredictionData){
+Radar_Error Radar_InitData(Radar_PredictionData_t *pPredictionData)
+{
 	Radar_Error Status = RADAR_ERROR_NONE;
-	pPredictionData->SpeedData.DeltaV=0;
-	pPredictionData->SpeedData.DeltaX=0;
-	pPredictionData->SpeedData.InitialDistance=0;
-	pPredictionData->SpeedData.InitialSpeed=0;
-	pPredictionData->Status=RADAR_PREDICTIONSTATUS_INVALID;
+	pPredictionData->SpeedData.DeltaV = 0;
+	pPredictionData->SpeedData.DeltaX = 0;
+	pPredictionData->SpeedData.InitialDistance = 0;
+	pPredictionData->SpeedData.InitialSpeed = 0;
+	pPredictionData->Status = RADAR_PREDICTIONSTATUS_INVALID;
 	return Status;
 }
